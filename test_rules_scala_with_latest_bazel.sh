fd201b879ef3f066d1f0a09154d6c8fe675c68b7
Or Shachar <ors@wix.com>
2020-12-01 20:18:17 +0200

    fix

--- test_rules_scala_with_latest_bazel.sh
+++ test_rules_scala_with_latest_bazel.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 export USE_BAZEL_VERSION="latest"
-"${dir}"/test_version.sh /test_rules_scala.sh $@
+"${dir}"/test_rules_scala.sh $@
